b405f4f9501a4b9112b799955e12239d8d4bf45d
FabiBr <fabian.br.1992@gmail.com>
2017-01-09 11:42:46 +0100

    init

---
+++ src/MLController.java
@@ -0,0 +1,18 @@
+import net.sf.javaml.classification.Classifier;
+import net.sf.javaml.classification.bayes.NaiveBayesClassifier;
+import net.sf.javaml.core.SparseInstance;
+import weka.core.Instance;
+
+public class MLController {
+
+	public static void main(String[] args) {
+		
+		Classifier bayes = new NaiveBayesClassifier(false, false, false);
+		bayes.
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
