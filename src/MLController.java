8929c19933acdcf576fa7252c77477803b95ae08
Marco Oliva <marco.oliva@kupferwerk.com>
2017-01-16 13:44:17 +0100

    Adds dataset reader

--- src/MLController.java
+++ src/MLController.java
@@ -1,16 +1,25 @@
 import net.sf.javaml.classification.Classifier;
 import net.sf.javaml.classification.bayes.NaiveBayesClassifier;
+import net.sf.javaml.core.Dataset;
 import net.sf.javaml.core.SparseInstance;
+import net.sf.javaml.tools.data.FileHandler;
 import weka.core.Instance;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MLController {
 
 	public static void main(String[] args) {
-		
-		Classifier bayes = new NaiveBayesClassifier(false, false, false);
-		bayes.
-		
-		
+
+		try {
+			Dataset data = FileHandler.loadDataset(new File("/Users/marco.oliva/Desktop/20170109-timeline-sample-twitter-bot.csv"), 0, ",");
+			System.out.println(data);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
 		// TODO Auto-generated method stub
 
 	}
